22d0ddc40559ca8d431f2e7ef7e0eb3ddca53863
Jorgel <jkeci15@epoka.edu.al>
2021-08-09 23:19:39 +0200

    Added Cart interface with some needed method headers

---
+++ src/main/java/interfaces/CartI.java
@@ -0,0 +1,15 @@
+package interfaces;
+import model.Product;
+import java.util.HashMap;
+
+public interface CartI {
+    void addProduct(Product product, double amount);
+    void removeProduct(Product product, double amount);
+    int getUniqueCount();
+    double getTotalCount();
+    HashMap<Product, Double> getCart();
+    Double getProductAmount(Product product);
+    boolean hasProduct(Product product);
+    void updateProductAmount(Product product, double newAmount);
+//    CartI cloneCart();
+}
