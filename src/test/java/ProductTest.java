a6f09407f5a824c8df43d72269f030793292f7f6
Jorgel <jkeci15@epoka.edu.al>
2021-08-09 23:13:57 +0200

    Added first test for Product class

---
+++ src/test/java/ProductTest.java
@@ -0,0 +1,12 @@
+import model.Product;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ProductTest {
+    @Test
+    void testProductCreation() throws Exception {
+        Product product = new Product('A', 50);
+        Assertions.assertEquals('A',product.getSku());
+        Assertions.assertEquals(50,product.getPrice());
+    }
+}
