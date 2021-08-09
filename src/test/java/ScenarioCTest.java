b597377cdd1dcd4bdcd688df5a679e3e6d71f555
Jorgel <jkeci15@epoka.edu.al>
2021-08-10 00:00:35 +0200

    Added test cases for three different scenarios explained in exercise sheet

---
+++ src/test/java/ScenarioCTest.java
@@ -0,0 +1,38 @@
+import PromotionTypes.nItemsPromotion;
+import PromotionTypes.togetherProductPromotion;
+import interfaces.PromotionI;
+import model.Cart;
+import model.Product;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class ScenarioCTest {
+    @Test
+    void testScenarioC() {
+        Product productA = new Product('A',50);
+        Product productB = new Product('B',30);
+        Product productC = new Product('C',20);
+        Product productD = new Product('D', 15);
+
+        Cart cart = new Cart();
+        cart.addProduct(productA, 3);
+        cart.addProduct(productB,5);
+        cart.addProduct(productC,1);
+        cart.addProduct(productD,1);
+
+        HashMap<Product,Double> discountedItems = new HashMap<>();
+        discountedItems.put(productC,(double)1);
+        discountedItems.put(productD,(double)1);
+        List<PromotionI> promotions = new ArrayList<>();
+        promotions.add(new nItemsPromotion(productA,3,130));
+        promotions.add(new nItemsPromotion(productB,2,45));
+        promotions.add(new togetherProductPromotion(discountedItems,30));
+        PromotionCalculator calculator = new PromotionCalculator();
+        Assertions.assertEquals(280, calculator.getDiscountedPrice(cart,promotions));
+
+    }
+}
